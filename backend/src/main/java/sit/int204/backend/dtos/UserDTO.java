77b1afd3c90ba42a1cd39a750a21802d2c63e5c5
Nanthawan <nanthawan.ice31@mail.kmutt.ac.th>
2023-09-06 22:03:04 +0700

    littlePsw

--- backend/src/main/java/sit/int204/backend/dtos/UserDTO.java
+++ backend/src/main/java/sit/int204/backend/dtos/UserDTO.java
@@ -17,7 +17,6 @@ public class UserDTO {
 
 
 
-    //TRIM
     public String getUsername() {
         return username;
     }
@@ -51,4 +50,38 @@ public class UserDTO {
         this.role = role;
     }
 
+    //password
+    public void setPassword(String password) {
+        // เช็คความยาว 8-14 ตัวอักษร
+        if (password.length() < 8 || password.length() > 14) {
+            throw new IllegalArgumentException("Password must be between 8 and 14 characters.");
+        }
+
+        // เช็คว่ามีอักขระตัวใหญ่
+        if (!password.matches(".*[A-Z].*")) {
+            throw new IllegalArgumentException("Password must contain at least one uppercase letter.");
+        }
+
+        // เช็คว่ามีอักขระตัวเล็ก
+        if (!password.matches(".*[a-z].*")) {
+            throw new IllegalArgumentException("Password must contain at least one lowercase letter.");
+        }
+
+        // เช็คว่ามีตัวเลข
+        if (!password.matches(".*\\d.*")) {
+            throw new IllegalArgumentException("Password must contain at least one digit.");
+        }
+
+        // เช็คว่ามีสัญลักษณ์พิเศษ
+        if (!password.matches(".*[@#$%^&+=].*")) {
+            throw new IllegalArgumentException("Password must contain at least one special character.");
+        }
+
+        // เช็คว่าไม่มีช่องว่าง
+        if (password.contains(" ")) {
+            throw new IllegalArgumentException("Password cannot contain spaces.");
+        }
+
+        this.password = password.trim();
+    }
 }
