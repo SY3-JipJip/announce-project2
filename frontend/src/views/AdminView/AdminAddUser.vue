e06cf32b43535b58880b2434e9453509abeea18a
korapinz <88102268+korapinz@users.noreply.github.com>
2023-09-18 21:00:49 +0700

    sprint2:lated

--- frontend/src/views/AdminView/AdminAddUser.vue
+++ frontend/src/views/AdminView/AdminAddUser.vue
@@ -34,9 +34,8 @@ const textAlertEmpty = ref('')
 const isShowingUNRequire = ref('')
 const usernameRequireText = ref('')
 const isShowingCPWRequire = ref(false)
-const isShowingPWRequire = ref(false)
 const isShowingEMRequire = ref(false)
-const passRequireText = ref('')
+const passRequireText = ref('must be 8-14 characters long, at least 1 of uppercase, lowercase, number and special characters')
 const confirmPassRequireText = ref('')
 const nameRequireText = ref('')
 const isShowingNameRequire = ref('')
@@ -48,12 +47,18 @@ const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%^#&*()_+{}\[\]
 const emailPattern = /^\S+@\S+\.\S+$/
 
 
+function removeSpace() {
+    username.value.trim()
+    password.value = password.value.replace(/\s+/g, ''); // ลบ space ออก
+    confirmPassword.value.trim()
+    email.value.trim()
+}
 
 
 //ถ้ากดปุ่ม cancel ก็จะมี pop up เด้งขึ้นมาถามว่า จะ cancel จริงไหม ถ้าจริง ก็จะกลับไปหน้า home
 const cancel =()=>{
     let result = confirm('Confirm to cancel ')
-    if(result)router.push({name : 'AdminAddUserView'})
+    if(result)router.push({name : 'AdminUserView'})
 }
 
 
@@ -86,28 +91,18 @@ const enableAdd = computed(()=>{
     }
     
 
-    // USERNAME UNIQUE   
-    const isUnique = oldUsers.value.find((user) => user.username === username.value);
-
-    if (isUnique) {
-        usernameRequireText.value = 'does not unique';
-        isShowingUNRequire.value = true;
-        console.log(username.value);
-    } else {
-        isShowingUNRequire.value = false;
-    }
-
+   
     // PASSWORD PATTERN
-    if (!passwordPattern.test(password.value) && password.value !== '' && password.value.length <= 8) {
-        passRequireText.value = 'Password must be between 8 and 14 characters'
-        isShowingPWRequire.value = true;
+    if(!passwordPattern.test(password.value) && password.value === 0 || password.value === ""){
+        passRequireText.value = 'must be 8-14 characters long, at least 1 of uppercase, lowercase, number and special characters'
+    }
+    else if (password.value !== '' && password.value.length < 8) {
+        passRequireText.value = 'Password size must be between 8 and 14'
     } else if (password.value.length > 0 && !passwordPattern.test(password.value)) {
         // Check if the user entered at least one special character
-        passRequireText.value = 'Password must include at least one special character';
-        isShowingPWRequire.value = true;
+        passRequireText.value = 'must be 8-14 characters long, at least 1 of uppercase, lowercase, number and special characters';
     } else {
         passRequireText.value = '';
-        isShowingPWRequire.value = false
     }
 
 
@@ -120,25 +115,26 @@ const enableAdd = computed(()=>{
     }
 
 
-    // Email Pattern
-    if (!emailPattern.test(email.value)) {
-        isShowingEMRequire.value = true;
-        if (email.value.length === 0) {
-            emailRequireText.value = "";
-        } else if (email.value.indexOf('@') === -1) {
-            emailRequireText.value = "Please enter a part following '@'.";
-        } else if (email.value.indexOf('@') !== -1) {
-            emailRequireText.value = "Please enter a part followed by '@'.";
-        } else if (email.value.indexOf('@') !== email.value.lastIndexOf('@')) {
-            emailRequireText.value = "A part following '@' should not contain the symbol '@'.";
-        } else {
-            emailRequireText.value = "Please include an '@' in the email address.";
+        // Email Pattern
+
+        if (!emailPattern.test(email.value) ) {
+            isShowingEMRequire.value = true;
+            if (email.value.length === 0) {
+                emailRequireText.value = '';
+            } else if (email.value.indexOf('@') === -1) {
+                emailRequireText.value = "Please enter a part following '@'.";
+            } else if (email.value.indexOf('@') !== email.value.lastIndexOf('@')) {
+                emailRequireText.value = "A part following '@' should not contain the symbol '@'.";
+            } else {
+                emailRequireText.value = "Please include an '@' in the email address.";
+            } 
+        }else {
+            emailRequireText.value = ""
+            isShowingEMRequire.value = false;
         }
-    } else {
-        isShowingEMRequire.value = false;
-    }
+
     
-    return isEmpty.value || isShowingUNRequire.value || isShowingPWRequire.value || isShowingCPWRequire.value ||  isShowingNameRequire.value || isShowingEMRequire.value 
+
 })
 
 
@@ -153,6 +149,7 @@ const submit = async () => {
     };
 
     try {
+        // ทำการ POST ข้อมูลใหม่ไปยัง API
         const response = await fetch(API_ROOT + '/api/users', {
             method: 'POST',
             headers: {
@@ -165,12 +162,44 @@ const submit = async () => {
             alert('Added user successfully');
             router.push('/admin/user');
         } else {
+            // ถ้าไม่สำเร็จในการเพิ่มผู้ใช้ใหม่
             alert('Unable to add new user!!!');
         }
     } catch (err) {
         errorMessage.value = err;
     }
+
+    // USERNAME UNIQUE
+    const isUniqueUsername = oldUsers.value.find((user) => user.username === newUser.username);
+    if (isUniqueUsername) {
+        usernameRequireText.value = 'does not unique';
+        isShowingUNRequire.value = true;
+    } else {
+        isShowingUNRequire.value = false;
+    }
+
+    // NAME UNIQUE
+    const isUniqueName = oldUsers.value.find((user) => user.name === newUser.name);
+    if (isUniqueName) {
+        nameRequireText.value = 'does not unique';
+        isShowingNameRequire.value = true;
+    } else {
+        isShowingNameRequire.value = false;
+    }
+
+    // EMAIL UNIQUE
+    const isUniqueEmail = oldUsers.value.find((user) => user.email === newUser.email);
+    if (isUniqueEmail) {
+        emailRequireText.value = 'does not unique';
+        isShowingEMRequire.value = true;
+    } else {
+        isShowingEMRequire.value = false;
+    }
 };
+
+
+ 
+
 
 
 
@@ -187,17 +216,17 @@ const submit = async () => {
                 <h1 class="text-3xl font-bold p-5">User Detail:</h1>
             </div>
 
-            <div class="w-full flex flex-col p-5 m-2">
+            <form @submit.prevent="submit" class="w-full flex flex-col p-5 m-2">
 
                 <div class="ann-item flex w-full flex-col space-y-5">
 
                     <!-- Username -->
                     <div class="w-full">
                         <p>Username<span class="text-red-700">*</span></p>
-                        <input type="text" placeholder="Type here" class="ann-username input input-bordered w-full max-w-xs" maxlength="45" v-model="username" required/>
+                        <input @input="removeSpace" type="text" placeholder="Type here" class="ann-username input input-bordered w-full max-w-xs" maxlength="45" v-model.trim="username" required/>
                         <span class="text-sm font-medium text-slate-700 ml-2">({{ username.length }}/45)</span> 
-                        <div class="flex gap-1 items-center justify-start text-red-600" v-if="isShowingUNRequire ">
-                            <span class="ann-error-username text-sm mt-3 w-1/2 m-1 text-red-500">{{ usernameRequireText }}</span> 
+                        <div class="flex gap-1 items-center justify-start text-red-600" v-show="isShowingUNRequire ">
+                            <span class="ann-error-username input:invalid text-sm mt-3 w-1/2 m-1 text-red-500">{{ usernameRequireText }}</span> 
                         </div>
                     </div>
                 
@@ -205,17 +234,17 @@ const submit = async () => {
                     <div class="w-full">
                         <p>Password<span class="text-red-700">*</span></p>
                         <div class="flex">
-                            <input type="password" placeholder="Type your password here..." class="ann-password input input-bordered textarea-md  w-3/5 max-w-xs" minlength="8" maxlength="14" v-model="password" required/>
+                            <input @input="removeSpace" type="password" placeholder="Type your password here..." class="ann-password input input-bordered textarea-md  w-3/5 max-w-xs" minlength="8" maxlength="14" v-model.trim="password" required/>
                             <div class="flex flex-row justify-between w-20">
                                 <span class="text-sm font-medium text-slate-700 ml-2 mt-3">({{ password.length }}/14) </span> 
                             </div>
                         </div>
                         <!-- information -->
-                        <div class="flex gap-1 items-center justify-start" v-if="isShowingPWRequire">
-                         <div class='ann-error-password text-sm mt-3 w-1/3 m-1 text-red-500 flex'>
-                            <span class="font-semibold">{{passRequireText}}</span>
+                        
+                         <div class='text-sm mt-3 w-1/3 m-1 text-red-500 flex'>
+                            <span class="ann-error-password font-semibold">{{passRequireText}}</span>
                          </div>
-                        </div>    
+                          
                 </div>
 
 
@@ -223,23 +252,23 @@ const submit = async () => {
                     <div class="w-full">
                         <p>Confirm password <span class="text-red-700">*</span></p>
                         <div class="flex">
-                            <input type="password" placeholder="Confirm your password here..." class="ann-confirm-password input input-bordered textarea-md  w-3/5 max-w-xs" minlength="8" maxlength="14" v-model="confirmPassword" required/>
+                            <input @input="removeSpace" type="password" placeholder="Confirm your password here..." class="ann-confirm-password input input-bordered textarea-md  w-3/5 max-w-xs" minlength="8" maxlength="14" v-model.trim="confirmPassword" required/>
                             <div class="flex flex-row justify-between w-20">
                                 <span class="text-sm font-medium text-slate-700 ml-2 mt-3">({{ confirmPassword.length }}/14) </span>
                             </div>
                         </div>
-                        <div class="flex gap-1 items-center justify-start" v-if="isShowingCPWRequire">
-                            <span class="ann-error-confirm-password font-serif text-sm mt-3 w-1/3 m-1 text-red-500">{{ confirmPassRequireText }}</span>
+                        <div class="flex gap-1 items-center justify-start" v-show="isShowingCPWRequire">
+                            <span class="ann-error-password text-sm mt-3 w-1/3 m-1 text-red-500 flex font-semibold">{{ confirmPassRequireText }}</span>
                         </div>
                     </div>
 
                     <!-- Name -->
                     <div class="w-full">
                         <p>Name <span class="text-red-700">*</span></p>
-                        <input  type="text" placeholder="Your name" class="ann-name  input input-bordered textarea-md  w-3/5 max-w-xs" maxlength="100" v-model="name" required/>
+                        <input type="text" placeholder="Your name" class="ann-name input input-bordered textarea-md  w-3/5 max-w-xs" maxlength="100" v-model.trim="name" required/>
                         <span class="text-sm font-medium text-slate-700 ml-2">({{ name.length }}/100)</span> 
-                        <div class="flex gap-1 items-center justify-start text-red-600" v-if="isShowingNameRequire ">
-                            <span class="text-sm mt-3 w-1/2 m-1 text-gray-500">{{ nameRequireText }}</span>
+                        <div class="flex gap-1 items-center justify-start text-red-600" v-show="isShowingNameRequire ">
+                            <span class="ann-error-name text-sm mt-3 w-1/2 m-1 text-red-500 font-semibold">{{ nameRequireText }}</span>
                         </div>
                     </div>
 
@@ -248,14 +277,14 @@ const submit = async () => {
                         <p>Email<span class="text-red-700">*</span></p>
 
                         <div class="flex">
-                            <input type="email" placeholder="example@example.com" class="ann-email input input-bordered w-full max-w-xs" maxlength="150"  v-model="email" required/>
+                            <input @input="removeSpace" type="email" placeholder="example@example.com" class="ann-email input input-bordered w-full max-w-xs" maxlength="150"  v-model.trim="email" required/>
                             <div class="flex flex-row justify-between w-20">
-                                <span class="ann-error-email text-sm font-medium text-slate-700 ml-2 mt-3">({{ email.length }}/150)</span>
+                                <span class="text-sm font-medium text-slate-700 ml-2 mt-3">({{ email.length }}/150)</span>
                             </div>
                         </div>
 
-                        <div class="flex gap-1 items-center justify-start text-red-600" v-if="isShowingEMRequire ">
-                            <span class="text-sm mt-3 w-1/2 m-1 text-red-500">{{ emailRequireText }}</span>
+                        <div class="flex gap-1 items-center justify-start text-red-600" v-show="isShowingEMRequire ">
+                            <span class="ann-error-email text-sm mt-3 w-1/2 m-1 text-red-500 font-semibold">{{ emailRequireText }}</span>
                         </div>
                     </div>
 
@@ -275,11 +304,11 @@ const submit = async () => {
 
                 <!-- SUBMIT & CANCEL -->
                 <div class="m-3 flex space-x-5 pb-3 mt-5">
-                    <button @click="submit" class="ann-button ann-submit ml-10 btn border border-green-500 bg-green-500 pl-5 pr-5" type="submit" :disabled="enableAdd">Save</button>
+                    <button class="ann-button ann-submit ml-10 btn border border-green-500 bg-green-500 pl-5 pr-5" type="submit" :class="enableAdd">add</button>
                     <button @click="cancel" class="ann-button ann-submit ml-10 btn border border-gray-500 bg-gray-500 pl-5 pr-5">Cancel</button>
                 </div>
 
-            </div>
+            </form>
     </div>
 </div>
 
--- frontend/src/views/AdminView/AdminEditUser.vue
+++ frontend/src/views/AdminView/AdminEditUser.vue
@@ -4,19 +4,28 @@ import { useRoute ,useRouter } from 'vue-router'
 import { getUsers } from '../../composable/getUsers';
 import { getUserById } from '../../composable/getUserById'
 import { formatDate } from '../../composable/formatDate'
+
 const API_ROOT = import.meta.env.VITE_API_ROOT
 const { params } = useRoute()
 const router = useRouter()
 
 
+//SHOW REQUIREMENT PASSWORD & EMAIL
+const isShowingUNRequire = ref('')
+const usernameRequireText = ref('')
+const isShowingEMRequire = ref(false)
+const nameRequireText = ref('')
+const isShowingNameRequire = ref('')
+const emailRequireText = ref('')
+
 //ข้อมูลเก่า
-const oldData = ref('')
+const oldData = ref([])
 
 //ข้อมูล user ทั้งหมด เพื่อเอา role
-const userDatas = ref('')
+const userDatas = ref([])
 
 //ข้อมูลของ user id นั้นๆ
-const userDetail = ref({})
+const userDetail = ref([])
 
 
 onMounted(async () => {
@@ -32,9 +41,10 @@ onMounted(async () => {
             "updatedOn" : userDetail.value.updatedOn
     }
 
-    console.log(oldData.value)
+    // console.log(oldData.value)
 
 })
+
 
 
 const uniqueRoles = computed(() => {
@@ -55,13 +65,8 @@ const cancle =()=>{
     router.push({name : 'AdminUserView'})
 }
 
-//จะ return ค่าเป็น Boolean ถ้า user เลือก category
-const isEdit = computed(()=>{
-    // console.log(edittingAnnouncement.value)
-    return  (edittingUser.value.username===undefined|| edittingUser.value.username==="" || edittingUser.value.name===undefined || edittingUser.value.name==="" || edittingUser.value.email===undefined || edittingUser.value.email==="" || edittingUser.value.role==="" || JSON.stringify(edittingUser.value) === JSON.stringify(oldData.value) )
-})
 
-//ค่าของ edittingAnnouncement ค่าเริ่มต้นจะเป็นของข้อมูลเดิม และเมื่อ user แก้ไข ตัวแปรก็จะเปลี่ยนค่าตามที่ user input ค่าเข้ามา
+// ค่าของ edittingAnnouncement ค่าเริ่มต้นจะเป็นของข้อมูลเดิม และเมื่อ user แก้ไข ตัวแปรก็จะเปลี่ยนค่าตามที่ user input ค่าเข้ามา
 const edittingUser = computed(()=>{
     console.log(edittingUser.value)
     return {
@@ -92,7 +97,7 @@ const submit = async () => {
 
         if (JSON.stringify(data) === JSON.stringify(oldData.value)) {
             router.push('/admin/user');
-        } else {
+        }else {
             try {
                 const response = await fetch(API_ROOT + '/api/users/' + params.id, {
                     method: "PUT",
@@ -108,16 +113,44 @@ const submit = async () => {
                 } else {
                     const errorData = await response.json();
                     alert(`Could not update data!!! : ${errorData.message}`);
-                    router.push('/admin/user');
                 }
             } catch (err) {
                 alert(`An error occurred: ${err}`);
                 router.push('/admin/user');
             }
         }
-    } else {
-        router.push('/admin/user');
-    }
+    } 
+
+// USERNAME UNIQUE   
+const isUnique = userDatas.value.find((user) => user.username === userDetail.value.username);
+
+if (isUnique) {
+    usernameRequireText.value = 'does not unique';
+    isShowingUNRequire.value = true;
+} else {
+    isShowingUNRequire.value = false;
+}
+
+
+
+// NAME UNIQUE   
+const isUniqueName = userDatas.value.find((user) => user.name === userDetail.value.name);
+if (isUniqueName) {
+    nameRequireText.value = 'does not unique';
+    isShowingNameRequire.value = true;
+} else {
+    isShowingNameRequire.value = false;
+}
+
+
+// EMAIL UNIQUE   
+const isUniqueEmail = userDatas.value.find((user) => user.email === userDetail.value.email);
+if (isUniqueEmail) {
+    emailRequireText.value = 'does not unique';
+    isShowingEMRequire.value = true;
+} else {
+    isShowingEMRequire.value = false;
+}
 };
 
 
@@ -126,31 +159,50 @@ const submit = async () => {
 <template>
 <div class="sm:ml-64 p-5">
 
-    <div class="border border-2">
+    <form @submit.prevent="submit" class="border border-2">
     <div class="w-full flex flex-row items-center">
         <img src="../../assets/images/user-avatar.png" class="h-7 ml-7">
         <h1 class="text-3xl font-bold p-5">User Detail:</h1>
     </div>
 
     <div class="w-full flex flex-col p-5">
-        <div class="detail">
-            <p>Username</p>
-            <input type="text" placeholder="Type here" class="ann-username input input-bordered w-full max-w-xs" v-model="userDetail.username" />
+        <!-- Username -->
+        <div class="w-full">
+            <p>Username<span class="text-red-700">*</span></p>{{ usernameRequireText }}
+            <input @input="removeSpace" type="text" placeholder="Type here" class="ann-username input input-bordered w-full max-w-xs" maxlength="45" v-model.trim="userDetail.username" required/>
+            <div class="flex gap-1 items-center justify-start text-red-600" v-show="isShowingUNRequire ">
+                <span class="ann-error-username input:invalid text-sm mt-3 w-1/2 m-1 text-red-500">{{ usernameRequireText }}</span> 
+            </div>
         </div>
-        <div class="detail">
-            <p>Name</p>
-            <input type="text" placeholder="Type here" class="ann-name input input-bordered w-full max-w-xs" v-model.trim="userDetail.name" />
+
+       <!-- Name -->
+       <div class="w-full">
+            <p>Name <span class="text-red-700">*</span></p>
+            <input type="text" placeholder="Your name" class="ann-name input input-bordered textarea-md  w-3/5 max-w-xs" maxlength="100" v-model.trim="userDetail.name" required/>
+            <div class="flex gap-1 items-center justify-start text-red-600" v-show="isShowingNameRequire ">
+                <span class="ann-error-name text-sm mt-3 w-1/2 m-1 text-red-500">{{ nameRequireText }}</span>
+            </div>
         </div>
-        <div class="detail">
-            <p>Email</p>
-            <input type="text" placeholder="Type here" class="ann-email input input-bordered w-full max-w-xs" v-model.trim="userDetail.email" />
+
+       <!-- Email -->
+       <div class="w-full">
+            <p>Email<span class="text-red-700">*</span></p>
+
+            <div class="flex">
+                <input @input="removeSpace" type="email" placeholder="example@example.com" class="ann-email input input-bordered w-full max-w-xs" maxlength="150"  v-model.trim="userDetail.email" required/>
+            </div>
+            <div class="flex gap-1 items-center justify-start text-red-600" v-show="isShowingEMRequire ">
+                <span class="ann-error-email text-sm mt-3 w-1/2 m-1 text-red-500 font-semibold">{{ emailRequireText }}</span>
+             </div>
         </div>
+
         <div class="form-control w-full max-w-xs detail">
             <p>Role</p>
             <select name="role" class="ann-role rounded-md p-1 border-4 border-blue-900" v-model="userDetail.role">
                 <option v-for="role in uniqueRoles" :value="role">{{ role }}</option>
             </select>
         </div>
+
         <div class="w-full">
             <div>Created On <span class="ann-created-on">{{ formatDate(userDetail.createdOn) }}</span></div>
             <div>Updated On <span class="ann-updated-on">{{ formatDate(userDetail.updatedOn) }}</span></div>
@@ -158,12 +210,12 @@ const submit = async () => {
     </div>
 
     <div class="flex justify-center">
-        <button @click="submit()" class="ann-button ann-submit ml-10 btn bg-green-600 pl-5 pr-5" :disabled="isEdit">submit</button>
+        <button class="ann-button ann-submit ml-10 btn bg-green-600 pl-5 pr-5">submit</button>
         <button @click="cancle()" class="ann-button ml-5 mb-6 btn buttonCancle" >Cancle</button>
     </div>
-
-    </div>
+</form>
 </div>
+
 </template>
  
 <style scoped>
