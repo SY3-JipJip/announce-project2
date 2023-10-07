e4ff564c5e0a10c1287a53aa038ebbf8869b72f7
korapinz <88102268+korapinz@users.noreply.github.com>
2023-10-07 16:01:46 +0700

    myfont-lated

--- frontend/src/composable/getNewToken.js
+++ frontend/src/composable/getNewToken.js
@@ -1,7 +1,7 @@
 const API_ROOT = import.meta.env.VITE_API_ROOT
 const getNewToken = async () => {
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_ROOT}/api/token`, {
+      const res = await fetch(`${API_ROOT}/api/token`, {
         headers: {
           Authorization: "Bearer " + localStorage.getItem('refreshToken')
         }
