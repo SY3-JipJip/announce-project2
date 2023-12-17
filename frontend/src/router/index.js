d7df7a07da1af69a2ed1f6a06e17602c19008586
korapinz <korapin.6945@kmutt.ac.th>
2023-12-17 11:52:40 +0700

    bruhh

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -90,7 +90,7 @@ router.beforeEach(async (to, from, next) => {
   if (to.path === "/") {
     next('/announcement');
 
-  } else if (localStorage.getItem("token") === null && localStorage.getItem("refreshToken")) {
+  } else if ((localStorage.getItem("token") === null )) {
 
     if (to.fullPath.includes("/admin/")) {
       alert("access deny! please login.");
@@ -98,6 +98,7 @@ router.beforeEach(async (to, from, next) => {
     } else {
       next();
     }
+    
   } else {
     const userRole = localStorage.getItem("userRole")
     if (to.fullPath.includes("/user/") && userRole !== 'admin') {
